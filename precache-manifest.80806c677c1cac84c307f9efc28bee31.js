e4179f098912d14fd9e52ad2393f3ce4395f580e
Wookyung Moon <wkmoon9601@gmail.com>
2020-05-04 15:03:07 -0300

    Updates

---
+++ precache-manifest.80806c677c1cac84c307f9efc28bee31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad18563715c5118addf077891a1dfd4",
+    "url": "/reactproject/movie_app/index.html"
+  },
+  {
+    "revision": "156e716bf12710f29988",
+    "url": "/reactproject/movie_app/static/css/main.46e09682.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/reactproject/movie_app/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactproject/movie_app/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156e716bf12710f29988",
+    "url": "/reactproject/movie_app/static/js/main.96d57d63.chunk.js"
+  },
+  {
+    "revision": "9c16a93aff1fb718d363",
+    "url": "/reactproject/movie_app/static/js/runtime-main.8a4548be.js"
+  }
+]);
